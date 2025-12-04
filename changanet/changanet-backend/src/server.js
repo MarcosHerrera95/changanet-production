d6b5468687487d6a38f4eb5aa7127587121e57f5
MarcosHerrera95 <makitosriver@gmail.com>
2025-12-04 12:14:25 -0300

    sesion google

--- changanet/changanet-backend/src/server.js
+++ changanet/changanet-backend/src/server.js
@@ -401,7 +401,21 @@ const rateLimiterMiddleware = (req, res, next) => {
 
 // Configuración de CORS para permitir solicitudes desde el frontend
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:5176', 'http://localhost:5177', 'http://localhost:5178', 'http://127.0.0.1:5173', 'http://127.0.0.1:5174', 'http://127.0.0.1:5175', 'http://127.0.0.1:5176', 'http://127.0.0.1:5177', 'http://127.0.0.1:5178'], // URLs del frontend en desarrollo
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'http://localhost:5175',
+    'http://localhost:5176',
+    'http://localhost:5177',
+    'http://localhost:5178',
+    'http://127.0.0.1:5173',
+    'http://127.0.0.1:5174',
+    'http://127.0.0.1:5175',
+    'http://127.0.0.1:5176',
+    'http://127.0.0.1:5177',
+    'http://127.0.0.1:5178',
+    'https://changanet-production-xgkf.onrender.com' // Frontend en producción
+  ],
   credentials: true, // Si necesitas enviar cookies/credenciales
 }));
 
