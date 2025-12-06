7ce05bd01f49c0538278bbbf50384b4cf745bf42
MarcosHerrera95 <makitosriver@gmail.com>
2025-12-05 23:58:13 -0300

    "Fix: allow cache-control header in CORS"

--- changanet/changanet-backend/src/server.js
+++ changanet/changanet-backend/src/server.js
@@ -421,7 +421,7 @@ app.use(cors({
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'cache-control'],
 }));
 
 app.use(rateLimiterMiddleware);
